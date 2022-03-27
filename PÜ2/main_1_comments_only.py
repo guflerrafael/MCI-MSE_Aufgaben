bd5d8277b4e2c091b3f5a93457964441d8d03f66
RICHI2409 <richi24091996@hotmail.com>
2022-03-27 20:28:22 +0200

    3.

--- PÜ2/main_1_comments_only.py
+++ PÜ2/main_1_comments_only.py
@@ -1,4 +1,19 @@
 # UC 2.0
 
 #%% UC 2.1 Einlesen der Daten
+#Alle Daten die während des Tests erfasst werden, werden eingelesen.
 
+#%% UC 2.2 Aufbereitung der Daten
+# Die Daten werden für die weitere Verarbeitung aufbereitet.
+ 
+# %% UC 2.3 Verarbeitung der Daten
+# Daten werden Verarbeitet und auf Fehler bzw. Abbruchkriterien geprüft.
+
+# %% UC2.4 Zusammenfassen der Daten
+# Alle Daten werden richtig Sortiert. 
+
+#%% UC 2.5 Visualiseren 
+#Die Daten werden mittels Diagrammen und anderen veranschulichungen Visualisert.
+
+#%% UC2.6 Speichern der Daten
+#Ein Ordner für den Patient wird erstellt und die gesamten Daten sowie die Diagramme werden Darin gespeichert.
