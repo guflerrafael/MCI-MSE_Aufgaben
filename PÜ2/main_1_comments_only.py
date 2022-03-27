a2bb10dae3ab30387bd003676efdde2a32918766
RICHI2409 <richi24091996@hotmail.com>
2022-03-27 20:50:57 +0200

    Aufgabe 3

--- PÜ2/main_1_comments_only.py
+++ PÜ2/main_1_comments_only.py
@@ -1,19 +1,22 @@
 # UC 2.0
 
 #%% UC 2.1 Einlesen der Daten
-#Alle Daten die während des Tests erfasst werden, werden eingelesen.
+#Alle Daten die während des Tests erfasst werden, werden eingelesen und in einem Array für die spätere Verarbeitung gespeichert.
 
-#%% UC 2.2 Aufbereitung der Daten
+#%% UC 2.2 Vorverarbeitung der Daten
 # Die Daten werden für die weitere Verarbeitung aufbereitet.
  
-# %% UC 2.3 Verarbeitung der Daten
-# Daten werden Verarbeitet und auf Fehler bzw. Abbruchkriterien geprüft.
+# %% UC 2.3 Analysieren der Daten auf Abbruchkriterien
+#  Die gesamten Daten werden auf die vorgegebenen Abbruchkriterien geprüft um die weitere Verarbeitung zu ermöglichen.
 
-# %% UC2.4 Zusammenfassen der Daten
-# Alle Daten werden richtig Sortiert. 
+# %% UC2.4 Erstellen einer Zusammenfassung
+# Alle Daten werden richtig Sortiert und eine Zusammenfassung wird erstellt. 
 
-#%% UC 2.5 Visualiseren 
-#Die Daten werden mittels Diagrammen und anderen Veranschulichungen visualisert.
+#%% UC 2.5 Visualisierung der Daten
+#Die Daten werden mittels Diagramme und anderen Veranschaulichungen Visualisiert, damit die Diagnostiker:in einen möglichst guten Überblick über die Werte hat.
 
-#%% UC2.6 Speichern der Daten
+#%% UC2.6 Manuelle Eingabe eines Abbruch-Kriteriums
+#Die Diagnostiker:in kann bei Notwendigkeit selber Abbruchkriterien implementieren.
+
+#%% UC2.7 Speichern der Daten
 #Ein Ordner wird erstellt und die gesamten Daten sowie die Diagramme werden darin gespeichert.
