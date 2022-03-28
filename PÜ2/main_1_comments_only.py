449451fe983b8d471d624894e0e2170fe61bf316
RICHI2409 <101810766+RICHI2409@users.noreply.github.com>
2022-03-28 14:34:10 +0200

    Update main_1_comments_only.py

--- PÜ2/main_1_comments_only.py
+++ PÜ2/main_1_comments_only.py
@@ -1,22 +1,22 @@
 # UC 2.0
 
 #%% UC 2.1 Einlesen der Daten
-#Alle Daten die während des Tests erfasst werden, werden eingelesen und in einem Array für die spätere Verarbeitung gespeichert.
+#Alle Daten, die während des Tests erfasst werden, werden eingelesen und in einem Array für die spätere Verarbeitung gespeichert.
 
 #%% UC 2.2 Vorverarbeitung der Daten
 # Die Daten werden für die weitere Verarbeitung aufbereitet.
  
 # %% UC 2.3 Analysieren der Daten auf Abbruchkriterien
-#  Die gesamten Daten werden auf die vorgegebenen Abbruchkriterien geprüft um die weitere Verarbeitung zu ermöglichen.
+#  Die gesamten Daten werden auf die vorgegebenen Abbruchkriterien geprüft, um die weitere Verarbeitung zu ermöglichen.
 
 # %% UC2.4 Erstellen einer Zusammenfassung
-# Alle Daten werden richtig Sortiert und eine Zusammenfassung wird erstellt. 
+# Alle Daten werden richtig sortiert und eine Zusammenfassung wird erstellt. 
 
 #%% UC 2.5 Visualisierung der Daten
-#Die Daten werden mittels Diagramme und anderen Veranschaulichungen Visualisiert, damit die Diagnostiker:in einen möglichst guten Überblick über die Werte hat.
+#Die Daten werden mittels Diagramme und anderen Veranschaulichungen visualisiert, damit die Diagnostiker:in einen möglichst guten Überblick über die Werte hat.
 
 #%% UC2.6 Manuelle Eingabe eines Abbruch-Kriteriums
-#Die Diagnostiker:in kann bei Notwendigkeit selber Abbruchkriterien implementieren.
+#Die Diagnostiker:in kann bei Notwendigkeit über die Kommandozeile Abbruchkriterien hinzufügen.
 
 #%% UC2.7 Speichern der Daten
 #Ein Ordner wird erstellt und die gesamten Daten sowie die Diagramme werden darin gespeichert.
