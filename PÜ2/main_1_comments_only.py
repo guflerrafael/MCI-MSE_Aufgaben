828d05707ec3578dde263de71fc01b0a52bc1d0e
guflerrafael <101810499+guflerrafael@users.noreply.github.com>
2022-03-29 11:48:18 +0200

    Update main_1_comments_only.py

--- PÜ2/main_1_comments_only.py
+++ PÜ2/main_1_comments_only.py
@@ -1,22 +1,23 @@
 # UC 2.0
 
-#%% UC 2.1 Einlesen der Daten
-#Alle Daten, die während des Tests erfasst werden, werden eingelesen und in einem Array für die spätere Verarbeitung gespeichert.
+# %% UC 2.1 Einlesen der Daten
+# Alle Daten, die während des Tests erfasst wurden, werden eingelesen und in einem Array für die spätere Verarbeitung gespeichert.
 
-#%% UC 2.2 Vorverarbeitung der Daten
+# %% UC 2.2 Vorverarbeitung der Daten
 # Die Daten werden für die weitere Verarbeitung aufbereitet.
  
 # %% UC 2.3 Analysieren der Daten auf Abbruchkriterien
-#  Die gesamten Daten werden auf die vorgegebenen Abbruchkriterien geprüft, um die weitere Verarbeitung zu ermöglichen.
+# Die gesamten Daten werden auf die vorgegebenen Abbruchkriterien geprüft, gegebenenenfalls als ungültig markiert, 
+# ansonsten für die weitere Verarbeitung zu gleichbelassen.
 
-# %% UC2.4 Erstellen einer Zusammenfassung
+# %% UC 2.4 Erstellen einer Zusammenfassung
 # Alle Daten werden richtig sortiert und eine Zusammenfassung wird erstellt. 
 
-#%% UC 2.5 Visualisierung der Daten
-#Die Daten werden mittels Diagramme und anderen Veranschaulichungen visualisiert, damit die Diagnostiker:in einen möglichst guten Überblick über die Werte hat.
+# %% UC 2.5 Visualisierung der Daten
+# Die Daten werden mittels Diagrammen und anderen Veranschaulichungen visualisiert, damit Diagnostiker:in einen guten Überblick über die Werte hat.
 
-#%% UC2.6 Manuelle Eingabe eines Abbruch-Kriteriums
-#Die Diagnostiker:in kann bei Notwendigkeit über die Kommandozeile Abbruchkriterien hinzufügen.
+# %% UC 2.6 Manuelle Eingabe eines Abbruch-Kriteriums
+# Diagnostiker:in kann bei Notwendigkeit über die Kommandozeile Abbruchkriterien hinzufügen und Daten als ungültig markieren.
 
-#%% UC2.7 Speichern der Daten
-#Ein Ordner wird erstellt und die gesamten Daten sowie die Diagramme werden darin gespeichert.
+# %% UC 2.7 Speichern der Daten
+# Ein Ordner wird erstellt und die gesamten Daten, sowie die Diagramme, werden darin gespeichert.
